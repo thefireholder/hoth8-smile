260ee4b04ea42e0c51eb6f98a232497d64f458a5
thefireholder <aaccjjt@hotmail.com>
2021-02-07 07:50:50 -0800

    disk saving done

--- components/screens/FileScreen.js
+++ components/screens/FileScreen.js
@@ -7,7 +7,8 @@ export default function notification({ route, navigation }) {
 const [a,setA] = React.useState('none');
 const [aa,setAA] = React.useState('none');
 const [n,setN] = React.useState(100);
-const [list,setList] = React.useState([]);
+const [clist,setClist] = React.useState([]);
+const [flist,setFlist] = React.useState([]);
 
 React.useEffect(()=>{
   (async ()=>{
@@ -19,15 +20,23 @@ React.useEffect(()=>{
 
     const bb1 = await FileSystem.readDirectoryAsync(FileSystem.cacheDirectory+bb[0]);
     setN(bb1.length);
-
-    setList(bb1);
+    setClist(bb1);
+    
+    const bb2 = await FileSystem.readDirectoryAsync(FileSystem.documentDirectory+b[0]);
+    setFlist(bb2);
   })();
 },[])
 
 const cachedelete = async () => {
   const dirs = await FileSystem.readDirectoryAsync(FileSystem.cacheDirectory);
   const files = await FileSystem.readDirectoryAsync(FileSystem.cacheDirectory+dirs[0]);
-  FileSystem.deleteAsync(FileSystem.cacheDirectory+'Camera'+'/'+files[0]);
+  FileSystem.deleteAsync(FileSystem.cacheDirectory+dirs[0]);
+}
+
+const filedelete = async () => {
+  const fdirs = await FileSystem.readDirectoryAsync(FileSystem.documentDirectory);
+  const ffiles = await FileSystem.readDirectoryAsync(FileSystem.documentDirectory+fdirs[0]);
+  FileSystem.deleteAsync(FileSystem.documentDirectory+fdirs[0]);
 }
 
 
@@ -37,17 +46,26 @@ const numbers = [1, 2, 3, 4, 5];
  <View style={{ flex: 1, alignItems:'center',justifyContent:'center' }}>
   <Text>a=</Text>
   <Text>{a}</Text>
+  <FlatList
+    data={flist}
+    renderItem={({item})=>(<Text>{item}</Text>)}
+    keyExtractor={item => item}
+  />
    <Text>b=</Text>
    <Text>{aa}</Text>
    <Text>{n}</Text>
    <FlatList
-     data={list}
+     data={clist}
      renderItem={({item})=>(<Text>{item}</Text>)}
      keyExtractor={item => item}
    />
    <Button
      title="delete all cache"
      onPress={cachedelete}
+   />
+   <Button
+     title="delete all perm"
+     onPress={filedelete}
    />
 </View>
  );
--- components/screens/PreviewScreen.js
+++ components/screens/PreviewScreen.js
@@ -3,36 +3,58 @@ import { StyleSheet, Text, View ,Image, Button, BackHandler, Alert} from'react-n
 import { Video, Audio } from 'expo-av';
 import * as FileSystem from 'expo-file-system';
 import * as VideoThumbnails from 'expo-video-thumbnails';
+import { CommonActions } from '@react-navigation/native';
+import moment from 'moment';
 
 export default function notification({ route, navigation }) {
-
-  // thumbnail
-  const [thumbnail, setThumbnail] = React.useState(null);
 
   React.useEffect(()=>{
     //set audio
     (async () => await Audio.setAudioModeAsync({playsInSilentModeIOS: true,}))();
-    return async ()=>{
-      //component will umount
-      const videoUri = route.params?.video.uri;
-      Alert.alert(videoUri);
-      if(videoUri) await FileSystem.deleteAsync(FileSystem.cacheDirectory+'Camera/'+videoUri);
-
-
-    }
   },[])
 
 
  const videoUri = route.params ? route.params.video.uri : 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4';
 
  const save = async () => {
-   // try {
-   //   const { uri } = await VideoThumbnails.getThumbnailAsync(photo,{time: 2000,});
-   //   setThumbnail(uri);
-   // } catch (e) {
-   //   console.warn(e);
-   // }
-  Alert.alert("saved")
+
+   //check for video uri
+   const videoUri = route.params?.video.uri;
+   if (!videoUri) return;
+
+   //create thumbnail
+   let thumbnailUri = null;
+   try {
+     const { uri } = await VideoThumbnails.getThumbnailAsync(videoUri,{time: 2000,});
+     thumbnailUri = uri;
+   } catch (e) {
+     console.warn(e);
+     return;
+   }
+   if (!thumbnailUri) return;
+
+
+   // export them to permanent directory
+   FileSystem.makeDirectoryAsync(FileSystem.documentDirectory+'Video', {intermediates:true})
+   FileSystem.copyAsync({
+     from: videoUri,
+     to: FileSystem.documentDirectory+'Video/video'+moment().format('YYYY-MM-DD_hh:mm:ssa')
+   })
+   FileSystem.copyAsync({
+     from: thumbnailUri,
+     to: FileSystem.documentDirectory+'Video/thumbnail'+moment().format('YYYY-MM-DD_hh:mm:ssa')
+   })
+
+   //go back to beginning
+   navigation.dispatch(
+     CommonActions.reset({
+       index: 1,
+       routes: [
+         { name: 'Question' }
+       ],
+     })
+   );
+
  };
 
   return (
