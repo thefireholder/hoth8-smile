5db8a212de68061c75ed689ae12d395c5163c0dd
thefireholder <aaccjjt@hotmail.com>
2021-02-07 05:25:28 -0800

    Camara and Video added

--- components/screens/CameraScreen.js
+++ components/screens/CameraScreen.js
@@ -1,20 +1,106 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+
+export default function CameraScreen({navigation}) {
+
+ const [hasPermission, setHasPermission] = useState(null);
+ const [cameraRef, setCameraRef] = useState(null)
+ const [type, setType] = useState(Camera.Constants.Type.back);
+ const [recording, setRecording] = useState(null);
+
+ //get user permission
+ useEffect(() => {
+   (
+     async () => {
+       const { status } = await Camera.requestPermissionsAsync();
+       setHasPermission(status === 'granted');
+     }
+   )
+   ();
+ }, []);
+
+ //show permission warning
+ if (hasPermission === null) {
+  return <View />;
+ }
+ if (hasPermission === false) {
+   return <Text>No access to camera</Text>;
+ }
+
+ return (
+   <View style={{ flex: 1 }}>
+    <Camera style={{ flex: 1 }} type={type} ref={ref => {
+      setCameraRef(ref);
+    }} autoFocus="on">
+     <View
+      style={{
+        flex: 1,
+        backgroundColor: 'transparent',
+        justifyContent: 'flex-end'
+      }}>
+       <TouchableOpacity
+        style={{
+          flex: 0.1,
+          alignSelf: 'flex-end'
+        }}
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+            ? Camera.Constants.Type.front
+            : Camera.Constants.Type.back
+          );
+      }}>
+      <Text style= {{fontSize:18,marginBottom:10,color:'white'}}>Flip</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{alignSelf: 'center'}} onPress={async() => {
+       if(cameraRef){
+         // let photo = await cameraRef.takePictureAsync('photo');
+         // navigation.navigate('Image',{'photo':photo});
 
 
-export default function CameraScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>CameraScreen</Text>
+        //recording
+        if(!recording){
+          setRecording(true);
+          let photo = cameraRef.recordAsync('photo',{maxDuration:3});
+
+          setTimeout(()=>{
+            cameraRef.stopRecording();
+            setRecording(null);
+          },5000)
+
+          photo = await photo;
+
+          navigation.navigate('Preview',{'photo':photo});
+        } else {
+            cameraRef.stopRecording()
+            setRecording(null);
+        }
+
+       }
+      }}>
+       <View style={{
+         borderWidth: 2,
+         borderRadius:"50%",
+         borderColor: recording ? "white":'red',
+         height: 50,
+         width:50,
+         display: 'flex',
+         justifyContent: 'center',
+         alignItems: 'center'}}
+       >
+         <View style={{
+           borderWidth: 2,
+           borderRadius:"50%",
+           borderColor: 'white',
+           height: 40,
+           width:40,
+           backgroundColor: 'white'}} >
+         </View>
+       </View>
+      </TouchableOpacity>
     </View>
-  );
+   </Camera>
+  </View>
+ );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- components/screens/PreviewScreen.js
+++ components/screens/PreviewScreen.js
@@ -1,20 +1,74 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View ,Image, Button} from'react-native';
+import { Video, Audio } from 'expo-av';
+// import * as FileSystem from 'expo-file-system';
+// import * as VideoThumbnails from 'expo-video-thumbnails';
+
+export default function notification({ route, navigation }) {
 
 
-export default function PreviewScreen() {
+ React.useEffect(()=>{
+   (
+     async ()=>{
+       await Audio.setAudioModeAsync({
+          playsInSilentModeIOS: true,
+        });
+     }
+   )();
+ },[])
+
+
+const [image, setImage] = React.useState(null);
+ const generateThumbnail = async () => {
+   const address = route.params.photo.uri;
+   try {
+     const { uri } = await VideoThumbnails.getThumbnailAsync(address,{time: 15000,});
+     setImage(uri);
+   } catch (e) {
+     console.warn(e);
+   }
+ };
+
+
+  // if no parameter was passed down, show the bunny video
+  if (route.params == undefined)
+  {
+    return (
+       <View style={{ flex: 1, alignItems:'center',justifyContent:'center' }}>
+         <Video
+           // source={{ uri: FileSystem.cacheDirectory+'Camera/07FF8564-392D-4246-A348-2904D5B75FA8.mov' }}
+           source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+           rate={1.0}
+           volume={1.0}
+           isMuted={false}
+           resizeMode="cover"
+           shouldPlay
+           isLooping
+           style={{ width: 300, height: 300 }}
+         />
+         <Button onPress={generateThumbnail} title="Generate thumbnail" />
+         {image && <Image source={{ uri: image }} style={{width: 200,height: 200}} />}
+         <Text>{image}</Text>
+      </View>
+    );
+  }
+
+// if parameter exist strip off photo
+ const { photo } = route.params;
+
+
   return (
-    <View style={styles.container}>
-      <Text>PreviewScreen</Text>
+     <View style={{ flex: 1, alignItems:'center',justifyContent:'center' }}>
+       <Video
+         source={{ uri: photo.uri }}
+         rate={1.0}
+         volume={1.0}
+         isMuted={false}
+         resizeMode="cover"
+         shouldPlay
+         isLooping
+         style={{ width: '100%', aspectRatio: 1 }}
+       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
